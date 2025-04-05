2a055deb51e7cdc8fd604daa03c4cde3aca6727d
Stephen Chung <spchung95@gmail.com>
2025-04-05 15:21:31 -0400

    cards

--- src/components/notes/index.tsx
+++ src/components/notes/index.tsx
@@ -1,48 +1,83 @@
 import { db } from "@/db/init";
 import { useAuth } from "@clerk/clerk-react";
 import { id } from "@instantdb/react";
-import { useEffect, useRef, useState } from "react";
+import { useEffect, useMemo, useRef, useState } from "react";
 
 export default function Notes() {
   const { userId } = useAuth();
+
   const [dialogOpen, setDialogOpen] = useState(false);
+  const [imageDialogOpen, setImageDialogOpen] = useState(false);
+
+  const dialogRef = useRef<HTMLDialogElement | null>(null);
+  const imageDialogRef = useRef<HTMLDialogElement | null>(null);
 
   const [text, setText] = useState("");
 
-  // useEffect(() => {
-  //   // transact! 🔥
-  //   console.log("adding");
-  //   db.transact(db.tx.notes[id()].update({ body: "first note" })).catch(
-  //     console.log
-  //   );
-  // }, [db]);
+  const {
+    isLoading,
+    error,
+    data: _data,
+  } = db.useQuery({
+    notes: {
+      $: {
+        where: {
+          createdBy: userId!,
+        },
+      },
+    },
+  });
 
-  const query = { notes: {} };
-  const { isLoading, error, data } = db.useQuery(query);
+  const onClose = () => {
+    dialogRef.current?.close();
+    setDialogOpen(false);
+    setText("");
+  };
+
+  const onCloseImageDialog = () => {
+    dialogRef.current?.close();
+    setDialogOpen(false);
+    setText("");
+  };
+
+  const notes = useMemo(() => {
+    return _data?.notes.sort(
+      (a, b) =>
+        new Date(b.createdAt).getTime() - new Date(a.createdAt).getTime()
+    );
+  }, [_data]);
 
   const handleAddNote = () => {
+    const body = text.trim();
+    if (!body) {
+      return;
+    }
     db.transact(
       db.tx.notes[id()].update({
-        body: text,
+        body,
         createdBy: userId!,
         createdAt: new Date().getTime(),
       })
-    ).catch(console.log);
+    )
+      .catch(console.log)
+      .then(() => onClose());
   };
 
-  const dialogRef = useRef(null);
-
   useEffect(() => {
-    const onClick = (e) => {
-      var rect = dialogRef.current.getBoundingClientRect();
-      var isInDialog =
+    const onClick = (e: MouseEvent) => {
+      const rect = dialogRef.current?.getBoundingClientRect();
+      if (!rect) {
+        return;
+      }
+
+      const isInDialog =
         rect.top <= e.clientY &&
         e.clientY <= rect.top + rect.height &&
         rect.left <= e.clientX &&
         e.clientX <= rect.left + rect.width;
+
       if (!isInDialog) {
-        setDialogOpen(false);
-        dialogRef.current.close();
+        onClose();
       }
     };
     window.addEventListener("mousedown", onClick);
@@ -51,25 +86,123 @@ export default function Notes() {
     };
   }, []);
 
+  useEffect(() => {
+    const onClick = (e: MouseEvent) => {
+      const rect = imageDialogRef.current?.getBoundingClientRect();
+      if (!rect) {
+        return;
+      }
+
+      const isInDialog =
+        rect.top <= e.clientY &&
+        e.clientY <= rect.top + rect.height &&
+        rect.left <= e.clientX &&
+        e.clientX <= rect.left + rect.width;
+
+      if (!isInDialog) {
+        onCloseImageDialog();
+      }
+    };
+    window.addEventListener("mousedown", onClick);
+    return () => {
+      window.removeEventListener("mousedown", onClick);
+    };
+  }, []);
+
+  useEffect(() => {
+    if (dialogOpen) {
+      dialogRef.current?.showModal();
+    } else {
+      dialogRef.current?.close();
+    }
+  }, [dialogOpen]);
+
+  useEffect(() => {
+    if (imageDialogOpen) {
+      imageDialogRef.current?.showModal();
+    } else {
+      imageDialogRef.current?.close();
+    }
+  }, [imageDialogOpen]);
+
+  const handleAddImage = () => {
+    console.log("todo");
+  };
+
   return (
     <div>
       <h1>Notes</h1>
+      {/* Actions */}
       <button onClick={() => setDialogOpen((prev) => !prev)}>New Note</button>
-      <dialog open={dialogOpen} ref={dialogRef}>
-        <div>Add Note</div>
-        <input value={text} onChange={(e) => setText(e.target.value)} />
-        <button onClick={handleAddNote}>Add</button>
+
+      {/* Add Note Dialog */}
+      <dialog ref={dialogRef}>
+        <div
+          style={{
+            width: "400px",
+            display: "flex",
+            flexDirection: "column",
+            gap: "2rem",
+          }}
+        >
+          <div>Add Note</div>
+          <textarea
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+            rows={5}
+          />
+          <button onClick={handleAddNote}>Add</button>
+        </div>
       </dialog>
+
+      {/* Add Image Dialog */}
+      <dialog ref={imageDialogRef}>
+        <div
+          style={{
+            width: "400px",
+            display: "flex",
+            flexDirection: "column",
+            gap: "2rem",
+          }}
+        >
+          <div>Add Image</div>
+          <input type="file" />
+          <button onClick={handleAddImage}>Add</button>
+        </div>
+      </dialog>
+
+      {/* Notes List */}
+      <div style={{ textAlign: "left" }}>NEWEST</div>
       <div
         style={{
-          display: "flex",
-          "flex-direction": "column",
+          display: "grid",
+          gridTemplateColumns: "1fr 1fr 1fr",
           gap: "2rem",
           padding: "2rem",
         }}
       >
-        {data?.notes.map((note) => <div key={note.id}>{note.body}</div>)}
+        {notes?.map((note) => (
+          <div
+            style={{
+              borderRight: "solid 1px",
+              borderLeft: "solid 1px",
+              borderBottom: "solid 1px",
+              borderTop: "solid 1px",
+              boxShadow: "0px 4px 6px rgba(0, 0, 0, 0.1)",
+
+              padding: "2rem",
+              margin: "auto",
+              // borderRadius: "12px",
+            }}
+            key={note.id}
+          >
+            {note.body}
+          </div>
+        ))}
       </div>
+      <div style={{ textAlign: "right" }}>OLDEST</div>
+
+      {/* Loading & Error */}
       {isLoading && "Loading..."}
       {error && "Error loading notes: " + error.message}
     </div>
