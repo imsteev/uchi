501feba601277716a6228ae2bb9d64214f729db2
Stephen Chung <spchung95@gmail.com>
2025-04-06 18:48:13 -0400

    VITE_INSTANT_APP_ID

--- src/db/init.ts
+++ src/db/init.ts
@@ -2,6 +2,6 @@ import { init } from "@instantdb/react";
 import schema from "../../instant.schema.ts";
 
 export const db = init({
-  appId: "28659409-309d-4398-9037-d3dd0f84abbb",
+  appId: import.meta.env.VITE_INSTANT_APP_ID,
   schema,
 });
