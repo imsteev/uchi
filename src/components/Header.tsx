5458f683d7d973fbea1a1e44ba54c10c3f0c6e74
Stephen Chung <spchung95@gmail.com>
2025-04-06 20:23:09 -0400

    update

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,16 +1,8 @@
-import {
-  SignedOut,
-  SignInButton,
-  SignedIn,
-  UserButton,
-} from "@clerk/clerk-react";
+import { SignedIn, UserButton } from "@clerk/clerk-react";
 
 export default function Header() {
   return (
     <header>
-      <SignedOut>
-        <SignInButton />
-      </SignedOut>
       <SignedIn>
         <UserButton />
       </SignedIn>
